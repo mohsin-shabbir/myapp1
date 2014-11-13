81017355d30074f3c98eab0963a55e069de98849
mohsin-shabbir <link2mohsin22@gmail.com>
2014-11-13 19:02:06 +0500

    as

--- configuration.php
+++ configuration.php
@@ -1,9 +1,19 @@
 <?php
 
 $conn=mysql_connect("localhost","localuser","mobit9gf7*uyu%^") or die("Connection Error");
-mysql_select_db("myapp1",$conn) or die("Database not Found");
+$rs = mysql_select_db("myapp1",$conn) or die("Database not Found");
 
 /*$conn=mysql_connect("localhost","root","") or die("Connection Error");
-mysql_select_db("myapp1",$conn) or die("Database not Found");*/
+$rs = mysql_select_db("myapp1",$conn) or die("Database not Found");*/
+if($rs)
+{
+	echo 'maiiltna';
+	exit;
+}
+else
+{
+	echo 'npot';
+	exit;
+}
 
 ?>
